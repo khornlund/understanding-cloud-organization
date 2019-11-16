f36d7c87b908737c6b838ac054c427f521849a3f
Karl Hornlund <karlhornlund@gmail.com>
2019-11-16 15:26:17 +1030

    Add script to get pseudolabels and symlink images

---
+++ setup-pseudo.sh
@@ -0,0 +1,7 @@
+./gdown.pl 'https://drive.google.com/open?id=1jPU9KKIo-QeinhmGPEq6J45Pqiht1rdp' ['data/raw/pseudo.csv']
+
+cd ~/bb/understanding-cloud-organization/data/raw
+mkdir joined_images/
+cd joined_images
+cp -rs ../train_images/* .
+cp -rs ../gibs/* .
